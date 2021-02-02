fb039415014a5fd0f0d5acd3a50b0723dbb7c209
zen138yanatib <64641188+zen138yanatib@users.noreply.github.com>
2021-02-02 16:39:02 +0700

    Update lab14_1.cpp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,9 +1,23 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+    int a = 5;
+    char b = 'A';
+    char &c = b;
+	int *x = &a;
+    char *y = &b;
+    int **z = &x; 
 
-	
-	
+    cout << a << " " << b << " " << c << " " << x << " " << (int*)y << " " << z << endl;
+    cout << &a << " " << (int*)&b << " " << (int*)&c << " " << &x << " " << &y << " " << &z << endl;
+    c = 'F';
+    cout << a << " " << b << " " << c << " " << x << " " << (int*)y << " " << z << endl;
+	*y = 'W';
+    cout << a << " " << b << " " << c << " " << x << " " << (int*)y << " " << z << endl;
+    *x = 6;
+    cout << a << " " << b << " " << c << " " << x << " " << (int*)y << " " << z << endl;
+    **z = 7;
+    cout << a << " " << b << " " << c << " " << x << " " << (int*)y << " " << z << endl; 
 	return 0;
 }
